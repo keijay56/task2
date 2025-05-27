eea175f15f1bddee1be4e4e4b622d3a68cd80410
keijay56 <derivv211@gmail.com>
2025-05-27 12:33:36 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def usd(request):
+    return render(request, 'usd/home.html')
+def get(self, request):
+    return render(request, 'usd/about.html')
+def get(self, request):
+    return render(request, 'usd/projects.html')
+
